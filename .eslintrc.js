06ca822581814f772544187f28febc70ad2e64d2
shanmao <wjs6298582@163.com>
2022-02-16 18:23:55 +0800

    聚光灯demo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,35 @@
 // CommonJs规范
 module.exports = {
-    // 标记当前代码最终的运行环境，eslint根据运行环境信息判断js的某个全局api（浏览器环境下的window,document）在当前环境是否可以使用
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "plugin:vue/essential",
-        "standard"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 12
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-    }
+  // 标记当前代码最终的运行环境，eslint根据运行环境信息判断js的某个全局api（浏览器环境下的window,document）在当前环境是否可以使用
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'arrow-parens': 0, // allow paren-less arrow functions
+    'generator-star-spacing': 0, // allow async-await
+    // allow debugger during development
+    'no-unused-vars': [1, {
+      vars: 'local', // 允许声明未使用变量
+      args: 'none' // 参数不检查
+    }],
+    semi: [0], // 关闭语句强制分号结尾
+    'no-multiple-empty-lines': [0, { max: 100 }], // 空行最多不能超过100行
+    'no-mixed-spaces-and-tabs': [0], // 关闭禁止混用tab和空格
+    'prefer-const': 1, // 是否优先使用const
+    'space-before-function-paren': [0, 'always'], // 函数定义时括号前面要不要有空格
+    'dot-notation': [0, { allowKeywords: true }] // 避免不必要的方括号
+  }
 };
---
+++ src/demo/spot-light.js
@@ -0,0 +1,210 @@
+import * as THREE from 'three'
+import * as dat from 'three/examples/jsm/libs/lil-gui.module.min.js'
+import Stats from 'three/examples/jsm/libs/stats.module.js'
+
+function init () {
+  const container = document.getElementById('container');
+  const stopMovingLight = false;
+  // create a scene to hold all elements,such as objects,cameras and lights
+  const scene = new THREE.Scene();
+  // create a camera,which defines where to look at
+  const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+  // create a render and set the size
+  const renderer = new THREE.WebGLRenderer();
+
+  // renderer.setClearColor(new THREE.Color('#ffffff', 1.0));
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFShadowMap;
+
+  // create and position the plane
+  const planeGeometry = new THREE.PlaneGeometry(100, 40, 1, 1);
+  const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
+  const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  plane.receiveShadow = true;
+
+  plane.rotation.x = -0.5 * Math.PI;
+  plane.position.x = 15;
+  plane.position.y = 0;
+  plane.position.z = 0;
+
+  scene.add(plane);
+
+  // create a cube
+  const cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
+  const cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xff3333 });
+  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  cube.castShadow = true;
+
+  // position the cube
+  cube.position.x = -4;
+  cube.position.y = 3;
+  cube.position.z = 0;
+
+  // add the cube to the scene
+  scene.add(cube);
+
+  const sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
+  const sphereMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff });
+  const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+
+  // position the sphere
+  sphere.position.x = 20;
+  sphere.position.y = 0;
+  sphere.position.z = 2;
+  sphere.castShadow = true;
+
+  // add the sphere to the scene
+  scene.add(sphere);
+
+  // position and point the camera to the center of the scene
+  camera.position.x = -35;
+  camera.position.y = 30;
+  camera.position.z = 25;
+  camera.lookAt(new THREE.Vector3(10, 0, 0));
+
+  // add subtle ambient lighting
+  const ambiColor = '#1c1c1c';
+  const ambientLight = new THREE.AmbientLight(ambiColor);
+  scene.add(ambientLight);
+
+  // add spotlight for a bit of light
+  const spotLight0 = new THREE.SpotLight(0xcccccc);
+  spotLight0.position.set(-40, 30, -10);
+  spotLight0.lookAt(plane);
+  scene.add(spotLight0);
+
+  // const target = new THREE.Object3D();
+  // target.position = new THREE.Vector3(5, 0, 0);
+
+  const pointColor = '#ffffff';
+  const spotLight = new THREE.SpotLight(pointColor);
+  spotLight.position.set(-40, 60, -10);
+  spotLight.castShadow = true;
+  spotLight.shadow.camera.near = 2;
+  spotLight.shadow.camera.far = 200;
+  spotLight.shadow.camera.fov = 30;
+  spotLight.target = plane;
+  spotLight.distance = 0;
+  spotLight.angle = 0.4;
+
+  scene.add(spotLight);
+
+  // add a small sphere simulating the pointlight
+  const sphereLight = new THREE.SphereGeometry(0.2);
+  const sphereLightMaterial = new THREE.MeshBasicMaterial({ color: 0xac6c25 });
+  const sphereLightMesh = new THREE.Mesh(sphereLight, sphereLightMaterial);
+  sphereLightMesh.castShadow = true;
+
+  sphereLightMesh.position.x = 3;
+  sphereLightMesh.position.y = 20;
+  sphereLightMesh.position.z = 3;
+
+  scene.add(sphereLightMesh);
+
+  // add the output of the renderer to the html element
+  container.appendChild(renderer.domElement);
+
+  // call the render function
+  let step = 0;
+
+  // used to determine the switch point for the light animation
+  let invert = 1;
+  let phase = 0;
+
+  const controls = {
+    rotationSpeed: 0.03,
+    bouncingSpeed: 0.03,
+    ambientColor: ambiColor,
+    pointColor: pointColor,
+    intensity: 1,
+    distance: 0,
+    exponent: 30,
+    angle: 0.1,
+    debug: false,
+    castShadow: true,
+    onlyShadow: false,
+    target: 'Plane',
+    stopMovingLight: false
+  }
+
+  const gui = new dat.GUI();
+
+  gui.add(controls, 'ambientColor').onChange((e) => {
+    ambientLight.color = new THREE.Color(e);
+  })
+
+  gui.add(controls, 'target', ['Plane', 'Sphere', 'Cube']).onChange(function (e) {
+    console.log(e);
+    switch (e) {
+      case 'Plane':
+        spotLight.target = plane;
+        break;
+      case 'Sphere':
+        spotLight.target = sphere;
+        break;
+      case 'Cube':
+        spotLight.target = cube;
+        break;
+    }
+  });
+
+  function initStats() {
+    const stats = new Stats();
+
+    stats.setMode(0); // 0: fps, 1: ms
+
+    // Align top-left
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.left = '0px';
+    stats.domElement.style.top = '0px';
+
+    document.getElementById('stats-output').appendChild(stats.domElement);
+
+    return stats;
+  }
+  const stats = initStats();
+
+  render();
+
+  function render() {
+    stats.update();
+    // rotate the cube around its axes
+    cube.rotation.x += controls.rotationSpeed;
+    cube.rotation.y += controls.rotationSpeed;
+    cube.rotation.z += controls.rotationSpeed;
+
+    // bounce the sphere up and down
+    step += controls.bouncingSpeed;
+    sphere.position.x = 20 + (10 * (Math.cos(step)));
+    sphere.position.y = 2 + (10 * Math.abs(Math.sin(step)));
+
+    // move the light simulation
+    if (!stopMovingLight) {
+      if (phase > 2 * Math.PI) {
+        invert = invert * -1;
+        phase -= 2 * Math.PI;
+      } else {
+        phase += controls.rotationSpeed;
+      }
+      sphereLightMesh.position.z = +(7 * (Math.sin(phase)));
+      sphereLightMesh.position.x = +(14 * (Math.cos(phase)));
+      sphereLightMesh.position.y = 10;
+
+      if (invert < 0) {
+        const pivot = 14;
+        sphereLightMesh.position.x = (invert * (sphereLightMesh.position.x - pivot)) + pivot;
+      }
+
+      spotLight.position.copy(sphereLightMesh.position);
+    }
+
+    // render using requestAnimationFrame
+    requestAnimationFrame(render);
+
+
+    renderer.render(scene, camera);
+  }
+}
+
+window.onload = init;
