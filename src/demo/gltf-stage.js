bf2271dc4267949265647b6b6a1d25df240f4b7a
shanmao <wjs6298582@163.com>
2022-04-09 21:29:14 +0800

    blender模型加载渲染优化

--- src/demo/gltf-stage.js
+++ src/demo/gltf-stage.js
@@ -30,6 +30,11 @@ function init () {
   // const gltfLoader = new GLTFLoader()
   // gltfLoader.setDRACOLoader(dracoLoader)
 
+  // 性能优化
+  // 使用spector.js查看性能，可安装浏览器插件或者通过github安装到项目中
+  // 将所有baked对象合并到一个几何体中，仅需调用一次渲染函数完成场景渲染，提高性能
+  // 可以先在blender中，将相机光源以及发光材质除外的物体全部复制到一个集合（collection）中，并合并
+
   // 加载baked纹理贴图
   const textureLoader = new THREE.TextureLoader()
   const bakedTexture = textureLoader.load('/textures/baked/stage.jpg')
@@ -50,21 +55,44 @@ function init () {
   //   }
   // })
 
+  // 通用baked材质
   const bakedMaterial = new THREE.MeshBasicMaterial({
     map: bakedTexture
   })
 
+  // 照明背景材质
+  const portalLightMaterial = new THREE.MeshBasicMaterial({ color: '0xffffe5' });
+
+
+  // 照明灯材质
+  const poleLightMaterial = new THREE.MeshBasicMaterial({ color: '0xffffe5' })
+
   gltfLoader.load('/models/stage/glTF-Binary/stage.glb', (gltf) => {
     console.log(gltf)
     // 为模型中的每个对象添加材质,并应用baked纹理贴图
-    gltf.scene.traverse((child) => {
-      child.material = bakedMaterial
-    })
-    gltf.scene.position.set(0, 0.01, 0)
+    // gltf.scene.traverse((child) => {
+    //   child.material = bakedMaterial
+    // })
+
+    // 合并优化后的场景不再需要遍历
+    const bakedMesh = gltf.scene.children.find(child => child.name === 'Cube011')
+
+    // 单独设置照明对象材质，添加发光效果
+    // 照明背景通过着色器添加效果
+    const portalLightMesh = gltf.scene.children.find(child => child.name === 'poleBackgroud')
+    const poleLightAMesh = gltf.scene.children.find(child => child.name === 'poleLightA')
+    const poleLightBMesh = gltf.scene.children.find(child => child.name === 'poleLightB')
+
+    bakedMesh.material = bakedMaterial
+    portalLightMesh.material = portalLightMaterial
+    poleLightAMesh.material = poleLightBMesh.material = poleLightMaterial
+
+    gltf.scene.position.set(0, -0.2, 0)
     scene.add(gltf.scene)
   })
 
-  camera.position.set(2.9, 2.7, 4.6)
+  // camera.position.set(2.9, 2.7, 4.6)
+  camera.position.set(0, 2.0, 5.6)
   camera.lookAt(new THREE.Vector3(10, 0, 0));
 
   container.appendChild(renderer.domElement);
